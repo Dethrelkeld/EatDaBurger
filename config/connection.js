de20e296fea05133d86998e9331885569baaa51c
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-02-23 11:21:51 -0600

    added jawsdb if statement

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
+const connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+  connection = mysql.createConnection({
   
-  password: 'ziggy',
-  database: 'burgers_db',
-});
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    
+    password: 'ziggy',
+    database: 'burgers_db',
+  });
+};
 
 // Make connection.
 connection.connect((err) => {
