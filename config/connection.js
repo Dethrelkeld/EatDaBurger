b4c5dc74b60d87398ef0ed591fbbdd5c4edd20fd
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-02-23 11:46:49 -0600

    changed connection typo

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 } else {
   connection = mysql.createConnection({
